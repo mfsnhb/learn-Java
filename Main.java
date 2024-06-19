61c5c20d79f87dc877fcfd84c8854452fae04b2f
Li Weihao <211870033@smail.nju.edu.cn>
2024-06-20 00:18:43 +0800

    test

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        double a, b, c;
+        b = 100.254;
+        System.out.println("b value :" + b);
+        
     }
 }
